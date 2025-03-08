aa26743b61943a9bee67a63a44ab6ec7f7c81db9
LolipopJ <42314340+LolipopJ@users.noreply.github.com>
2025-03-08 22:22:19 +0800

    feat(progress): show loading progress if route switch is slow

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,11 @@
 import "./src/styles/global.css";
+
+import nProgress from "nprogress";
+
+export const onRouteUpdateDelayed = () => {
+  nProgress.start();
+};
+
+export const onRouteUpdate = () => {
+  nProgress.done();
+};
